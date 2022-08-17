39ecb7b00051a3533995880464197c9f9b20eb5e
Yann Guillon <yann@dockdev.com>
2022-08-17 11:05:18 +0200

    contract update + instructions

--- Readme.md
+++ Readme.md
@@ -1,13 +1,13 @@
 <img src="https://svgur.com/i/jYf.svg" width="200"/>
 
 ## We empower ✊ people to fundraise for charities and individuals
-####Trusted, secure and decentralised
+### Trusted, secure and decentralised
 
 ### This [master contract](./socialclain.sol) can be found live on the Polygon Mumbai Testnet :
-[0x100Fd5DC88898Fd061078579CACCD431FDEE72fA](https://mumbai.polygonscan.com/address/0x100Fd5DC88898Fd061078579CACCD431FDEE72fA)
+[0x0a07E53cbF44B50e92F81594c5AD74E0b2D2d452](https://mumbai.polygonscan.com/address/0x0a07E53cbF44B50e92F81594c5AD74E0b2D2d452)
 
 ### Disclamer
-TODO
+
 [comment]: <> (This contract requires a transfer of $LINK to operate &#40;0.1 for the `requestVerification&#40;&#41;` method and 0.1 per each call of the `verify&#40;&#41;` method&#41;)
 
 [comment]: <> (⚠️ Do not transfer $LINK directly to the contract, use the [ERC677 transferAndCall&#40;&#41; method]&#40;https://github.com/ethereum/EIPs/issues/677&#41;)
@@ -16,25 +16,23 @@ TODO
 The recommended way of interaction with the contract is to use the following dapp :
 [socialclaim.nescrypto.com](https://socialclaim.nescrypto.com)
 
-### Manual usage for URL verification
-TODO
+### Manual usage for wallet creation
 
-[comment]: <> (- 1:  `transferAndCall&#40;&#41;` to transfer $LINK tokens to the contract balance)
+- 1:  `transferAndCall()` to transfer $LINK tokens to the contract balance
 
-[comment]: <> (- 2: `requestVerification&#40;&#41;` to create the request)
+- 2: `requestWalletCreation()` to create the wallet
 
-[comment]: <> (- 3: subscribe for the `ValidationUpdate&#40;&#41;` event to get the randomly-generated challenge)
+- 3: subscribe to the `WalletCreationUpdate()` and `WalletCreated()` events to get the wallet information
 
-[comment]: <> (- 4:  `verify&#40;&#41;` to finalize the process)
+### Manual usage for withdrawal
+- 1:  `transferAndCall()` to transfer $LINK tokens to the contract balance
 
-[comment]: <> (- 5: subscribe to the `VerificationResult&#40;&#41;` event to get the verification result)
+- 2:  `requestWithdrawal()` to initialize the withdrawal process and get the challenge
 
-### Manual usage for getting a verification report
-TODO
+- 3:  `withdraw()` to process the withdrawal
 
-[comment]: <> (- 1: `getVerificationsForAddress&#40;address&#41;` with a valid BSC Testnet address)
+- 4: subscribe to the `WithdrawalUpdate()` and `WithdrawalResult()` events to get the withdrawal information
 
-[comment]: <> (- 2: subscribe to the `VerificationForAddress&#40;&#41;` event to get each verification)
 
 ### Notes if you wish to deploy this contract 
 - at deploy the contract creates a [VRF subscription](https://vrf.chain.link), its recommended to manually transfer at least 1 $LINK at deploy, to avoid issues with getting "pending" stuck RNG requests
--- socialclaim.sol
+++ socialclaim.sol
@@ -6,6 +6,7 @@ import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 abstract contract ERC677Receiver {
     /**
@@ -21,51 +22,79 @@ abstract contract ERC677Receiver {
     ) external virtual;
 }
 
-contract Verified is VRFConsumerBaseV2, ChainlinkClient, ERC677Receiver {
-    address linkTokenContract = 0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06;
+contract SocialClaim is VRFConsumerBaseV2, ChainlinkClient, ERC677Receiver, Ownable {
+    address linkTokenContract = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
     uint256 constant NULL = 0;
 
     // VRF
     VRFCoordinatorV2Interface COORDINATOR;
     LinkTokenInterface LINKTOKEN;
-    uint64 public s_subscriptionId;
-    address vrfCoordinator = 0x6A2AAd07396B36Fe02a22b33cf443582f682c82f;
-    bytes32 keyHash = 0xd4bb89654db74673a187bd804519e65e3f71a52bc55f11da7601a13dcf505314;
+    uint64 public VRFSubscriptionId;
+    address vrfCoordinator = 0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed;
+    bytes32 keyHash = 0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
     uint32 callbackGasLimit = 200000;
     uint16 requestConfirmations = 3;
     uint256 fee = 0.1 * 10 ** 18;
 
-    // ORACLE
-    address private oracle = 0xc897AB197611d16e4ABD18878aba8c85c8f370a9;
-    bytes32 private jobId = "fb4b27d00d704c3da5e16282e705f190";
-    uint256 oracle_fee = 0.1 * 10 ** 18;
+    // Withdrawal Oracle
+    address private withdrawalOracle = 0xFA776B1C972578c034B88e880F2F65729b43e9B0;
+    bytes32 private withdrawalJobId = "154067c0791e4b738cbc7edde49139be";
+    uint256 withdrawalOracleFee = 0.1 * 10 ** 18;
 
-    // VERIFIED SPECIFIC
+    // walletManagement Oracle
+    address private walletManagementOracle = 0x8B0376CF8CAcA511bB4F84b844f001B80263dfCE;
+    bytes32 private walletManagementJobId = "c5a0f8caea544d75b03fb8ada31a1c65";
+    uint256 walletManagementOracleFee = 0.1 * 10 ** 18;
+
+    // SOCIALCLAIM SPECIFIC
+
+    string public validSelector;
     struct Request
     {
         string URL;
+        bytes32 walletID;
         string selector;
         uint256 challenge;
-        uint256 balance;
+        address recipient;
     }
 
-    struct Verification
+    struct Transfer
     {
-        string timestamp;
-        string URL;
-        string selector;
+        bytes32 walletID;
+        address recipient;
     }
 
-    mapping (address => Request) requests;
-    mapping (address => Verification[]) allVerifications;
+    struct Balance
+    {
+        uint256 amount;
+    }
+
+    struct Wallet
+    {
+        string URL;
+    }
+
+    mapping (address => Balance) balances;
+    mapping (bytes32 => Wallet) wallets;
+
     mapping (uint256 => address) VRFRequestIds;
-    mapping (bytes32 => address) oracleRequestIds;
+
+    mapping (address => Request) creationRequests;
+    mapping (bytes32 => address) creationRequestIds;
+
+    mapping (address => Request) withdrawalRequests;
+    mapping (bytes32 => address) withdrawalRequestIds;
+
+    mapping (address => Transfer) transfers;
+    mapping (bytes32 => address) transferRequestIds;
 
     // EVENTS
-    event ValidationUpdate(address requester, uint256 challenge);
-    event VerificationResult(address requester, bool verified, string URL, string selector);
-    event VerificationForAddress(address requester, string verificationTimestamp, string URL, string selector);
+
     event PaymentSet(address requester, uint256 balance);
+    event WithdrawalUpdate(address requester, uint256 challenge);
+    event WithdrawalResult(address requester, uint256 status);
+    event WalletCreationUpdate(address requester);
+    event WalletCreated(address requester, bytes32 walletAddress);
 
     using Chainlink for Chainlink.Request;
     constructor() VRFConsumerBaseV2(vrfCoordinator) {
@@ -75,29 +104,28 @@ contract Verified is VRFConsumerBaseV2, ChainlinkClient, ERC677Receiver {
         createNewSubscription();
     }
 
-    function getVerificationsForAddress(address addr) public {
-        Verification[] memory verificationsForAddr = allVerifications[addr];
-        uint arrayLength = verificationsForAddr.length;
-        for (uint i=0; i<arrayLength; i++) {
-            emit VerificationForAddress(addr, verificationsForAddr[i].timestamp,
-                verificationsForAddr[i].URL,
-                verificationsForAddr[i].selector);
-        }
+    function setValidSelector(string calldata selector) public onlyOwner {
+        validSelector = selector;
     }
 
-    function requestVerification(string calldata URL, string calldata selector) public {
-        Request storage rq = requests[msg.sender];
-        if (rq.balance < fee)
-            revert("Please send 0.1 LINK (request) and 0.1 LINK (per 'verify' call) to this contract address");
-        rq.balance -= fee;
-        rq.URL = URL;
-        rq.selector = selector;
+    function requestWithdrawal(bytes32 walletID) public {
+        Wallet memory w = wallets[walletID];
+        if (bytes(w.URL).length == NULL)
+            revert("Wallet doesn't exist");
+        Balance storage balance = balances[msg.sender];
+        Request storage rq = withdrawalRequests[msg.sender];
+        if (balance.amount < fee)
+            revert("Please send 0.1 LINK for wallet creation to this contract address");
+        balance.amount -= fee;
+        rq.URL = w.URL;
+        rq.walletID = walletID;
         rq.challenge = NULL;
-        emit ValidationUpdate(msg.sender, rq.challenge);
-        LINKTOKEN.transferAndCall(address(COORDINATOR), fee, abi.encode(s_subscriptionId));
+
+        emit WithdrawalUpdate(msg.sender, rq.challenge);
+        LINKTOKEN.transferAndCall(address(COORDINATOR), fee, abi.encode(VRFSubscriptionId));
         uint256 requestId = COORDINATOR.requestRandomWords(
             keyHash,
-            s_subscriptionId,
+            VRFSubscriptionId,
             requestConfirmations,
             callbackGasLimit,
             1
@@ -105,14 +133,28 @@ contract Verified is VRFConsumerBaseV2, ChainlinkClient, ERC677Receiver {
         VRFRequestIds[requestId] = msg.sender;
     }
 
+    function requestWalletCreation(string calldata URL) public {
+        Request storage rq = creationRequests[msg.sender];
+        Balance storage balance = balances[msg.sender];
+        if (balance.amount < walletManagementOracleFee)
+            revert("Please send 0. LINK (request) and 0.1 LINK (per 'verify' call) to this contract address");
+        balance.amount -= walletManagementOracleFee;
+        rq.URL = URL;
+        emit WalletCreationUpdate(msg.sender);
+        Chainlink.Request memory oracleRequest = buildChainlinkRequest(walletManagementJobId, address(this), this.fulfill.selector);
+        oracleRequest.add("url", URL);
+        bytes32 oracleRequestId = sendChainlinkRequestTo(walletManagementOracle, oracleRequest, walletManagementOracleFee);
+        creationRequestIds[oracleRequestId] = msg.sender;
+    }
+
     function onTokenTransfer(
         address sender,
         uint256 value,
         bytes calldata data
     ) external override linkTokenOnly {
-        Request storage rq = requests[sender];
-        rq.balance += value;
-        emit PaymentSet(sender, rq.balance);
+        Balance storage balance = balances[sender];
+        balance.amount += value;
+        emit PaymentSet(sender, balance.amount);
     }
 
     modifier linkTokenOnly() {
@@ -125,50 +167,86 @@ contract Verified is VRFConsumerBaseV2, ChainlinkClient, ERC677Receiver {
         uint256[] memory randomWords
     ) internal override {
         address initiator = VRFRequestIds[requestId];
-        requests[initiator].challenge = randomWords[0];
-        emit ValidationUpdate(initiator, requests[initiator].challenge);
+        withdrawalRequests[initiator].challenge = randomWords[0];
+        emit WithdrawalUpdate(initiator, withdrawalRequests[initiator].challenge);
     }
 
     function createNewSubscription() private {
         address[] memory consumers = new address[](1);
         consumers[0] = address(this);
-        s_subscriptionId = COORDINATOR.createSubscription();
-        COORDINATOR.addConsumer(s_subscriptionId, consumers[0]);
+        VRFSubscriptionId = COORDINATOR.createSubscription();
+        COORDINATOR.addConsumer(VRFSubscriptionId, consumers[0]);
     }
 
-    function verify() public
+    function withdraw(address recipient) public
     {
-        Request storage rq = requests[msg.sender];
+        Request storage rq = withdrawalRequests[msg.sender];
+        Balance storage balance = balances[msg.sender];
+        if (recipient != address(recipient))
+            revert("Invalid ERC20 Address provided");
+        if (bytes(validSelector).length == NULL)
+            revert("Valid selector not set");
+        if (balance.amount < withdrawalOracleFee)
+            revert("Please send 0.1 LINK (withdrawal request) and 0.1 LINK (per 'withdraw' call) to this contract address");
+        balance.amount -= withdrawalOracleFee;
+        rq.recipient = recipient;
 
-        if (rq.balance < fee)
-            revert("Please send LINK to this contract address : 0.1 LINK (for the request) and 0.1 LINK (per 'verify' call)");
-        rq.balance -= fee;
-        if (rq.challenge == NULL)
-            revert("Please wait for the challenge to be generated");
-        if (bytes(rq.URL).length == NULL)
-            revert("Missing required parameters for verification : URL)");
-        if (bytes(rq.selector).length == NULL)
-            revert("Missing required parameters for verification : selector)");
-
-        Chainlink.Request memory oracle_request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
-        oracle_request.add("url", rq.URL);
-        oracle_request.add("selector", rq.selector);
-        oracle_request.add("challenge", Strings.toString(rq.challenge));
-        bytes32 oracleRequestId = sendChainlinkRequestTo(oracle, oracle_request, oracle_fee);
-        oracleRequestIds[oracleRequestId] = msg.sender;
+        Chainlink.Request memory withdrawalOracleRequest = buildChainlinkRequest(withdrawalJobId, address(this), this.fulfill.selector);
+        withdrawalOracleRequest.add("url", rq.URL);
+        withdrawalOracleRequest.add("selector", validSelector);
+        withdrawalOracleRequest.add("challenge", Strings.toString(rq.challenge));
+        withdrawalOracleRequest.add("walletID", bytes32ToString(rq.walletID));
+        withdrawalOracleRequest.add("recipient", toAsciiString(rq.recipient));
+        bytes32 oracleRequestId = sendChainlinkRequestTo(withdrawalOracle, withdrawalOracleRequest, withdrawalOracleFee);
+        withdrawalRequestIds[oracleRequestId] = msg.sender;
     }
 
-    function fulfill(bytes32 _requestId, bool _value) public recordChainlinkFulfillment(_requestId)
+    function fulfill(bytes32 _requestId, bytes32 _value) public recordChainlinkFulfillment(_requestId)
     {
-        address initiator = oracleRequestIds[_requestId];
-        if (_value == true) {
-            Verification memory vr = Verification({URL: requests[initiator].URL, selector: requests[initiator].selector, timestamp: Strings.toString(block.timestamp) });
-            Verification[] storage verifications = allVerifications[initiator];
-            verifications.push(vr);
+        address creatorInitiator = creationRequestIds[_requestId];
+        address withdrawerInitiator = withdrawalRequestIds[_requestId];
+
+        if (creatorInitiator != address(0)) {
+            Wallet memory wt = Wallet({URL: creationRequests[creatorInitiator].URL});
+            wallets[_value] = wt;
+            delete creationRequests[creatorInitiator];
+            delete creationRequestIds[_requestId];
+            emit WalletCreated(creatorInitiator, _value);
+        } else if (withdrawerInitiator != address(0)) {
+            emit WithdrawalResult(withdrawerInitiator, uint(_value));
+            delete withdrawalRequests[withdrawerInitiator];
+            delete withdrawalRequestIds[_requestId];
         }
-        emit VerificationResult(initiator, _value, requests[initiator].URL, requests[initiator].selector);
-        requests[initiator].URL = "";
-        requests[initiator].selector = "";
-        requests[initiator].challenge = NULL;
+    }
+
+    // UTILS
+
+    function toAsciiString(address x) internal pure returns (string memory) {
+        bytes memory s = new bytes(40);
+        for (uint i = 0; i < 20; i++) {
+            bytes1 b = bytes1(uint8(uint(uint160(x)) / (2**(8*(19 - i)))));
+            bytes1 hi = bytes1(uint8(b) / 16);
+            bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
+            s[2*i] = char(hi);
+            s[2*i+1] = char(lo);
+        }
+        return string(s);
+    }
+
+    function bytes32ToString(bytes32 _bytes32) public pure returns (string memory) {
+        uint8 i = 0;
+        while(i < 32 && _bytes32[i] != 0) {
+            i++;
+        }
+        bytes memory bytesArray = new bytes(i);
+        for (i = 0; i < 32 && _bytes32[i] != 0; i++) {
+            bytesArray[i] = _bytes32[i];
+        }
+        return string(bytesArray);
+    }
+
+    function char(bytes1 b) internal pure returns (bytes1 c) {
+        if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
+        else return bytes1(uint8(b) + 0x57);
     }
 }
